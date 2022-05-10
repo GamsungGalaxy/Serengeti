630a37f62b8b441ab7b2a975e56aa2aad0970ec4
Kevin <>
2022-05-10 16:17:20 -0700

    Initial commit; install dependencies
    Co-authored-by: Sam Carter <65371360+cartersamj@users.noreply.github.com>
    Co-authored-by: Chris LeBrett <100822842+fscgolden@users.noreply.github.com>
    Co-authored-by: Young Kim <101305992+youngs-kim@users.noreply.github.com>
    Co-authored-by: Kevin HoEun Lee <14950124+khobread@users.noreply.github.com>

---
+++ __tests__/react.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import App from '../client/App.jsx';
+import { render } from '@testing-library/react';
+
+describe('Unit testing React components', () => {
+  
+  // add two tests here
+  describe('FIRST COMPONENT', () => {
+    
+  })
+
+
+
+
+
+})
--- client/index.js
+++ client/index.js
@@ -2,13 +2,18 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
 import React from 'react';
-import { render } from 'react-dom';
+// import { render } from 'react-dom';
 import App from './App.jsx';
-
+import {createRoot} from 'react-dom/client'
 //our entry point  which allows us to include all the react components
 //building block for the bundle
 //contains all react components
-render(
-  <App />,
-  document.getElementById('root'),
-);
+// render(
+//   <App />,
+//   document.getElementById('root'),
+// );
+
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
