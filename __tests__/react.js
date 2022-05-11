006406d13f8763bde4c00e55b1d14aa8c0f6a781
Kevin <>
2022-05-10 17:13:14 -0700

    Setup Jest; Create (2) front-end tests

--- __tests__/react.js
+++ __tests__/react.js
@@ -1,16 +1,53 @@
 import React from 'react';
 import App from '../client/App.jsx';
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import Nav from '../client/components/Nav';
+import SearchBookRow from '../client/components/SearchBookRow.jsx';
 
 describe('Unit testing React components', () => {
-  
-  // add two tests here
-  describe('FIRST COMPONENT', () => {
+  describe('NavBar', () => {
+    let nav;
+
+    beforeAll(() => {
+      nav = render(<Nav/>);
+    });
     
-  })
+    test('Renders the nav bar with four(4) components: Home, My Books, Find Books, Log out', () => {
+      expect(screen.getByRole('nav-items')).toContainBLAHBLAHBLAH;
+
+      fireEvent.click();
+
+      expect(nav.getByRole('ul').nextSibling).toHaveTextContent('Home');
+      expect(nav.getByRole('ul').nextSibling).toHaveTextContent('My Books');
+      expect(nav.getByRole('ul').nextSibling).toHaveTextContent('Find Books');
+      expect(nav.getByRole('ul').nextSibling).toHaveTextContent('Log out');
+      
+      
+    });
+  });
+
+  describe('SearchBookRow', () => {
+    let searchBar;
+
+    beforeAll(() => {
+      searchBar = render(<SearchBookRow />);
+    });
+
+    test('Renders search bar with user input', () => {
+
+    });
+  });
 
 
+describe('MyBookRow', () => {
+    let myBookRow
 
-
-
-})
+    beforeAll(() => {
+      myBookRow = render(<MyBookRow/>);
+    });
+    test('Renders if delete book functions properly', () => {
+        
+    });
+  });
+    
+});
